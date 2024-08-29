5b7e9f18a1bd8142c0c37a2d2220f09bd3849524
AlexSerg01 <alex.box.work@icloud.com>
2024-08-29 13:40:04 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,32 +1,20 @@
-
 import { TodayWaterList } from "../../components/TodayWaterList/TodayWaterList";
-import Header from "../../components/Header/Header";
-import DailyNorma from "../../components/DailyNorma/DailyNorma";
-import WaterRatioPanel from "../../components/WaterRatioPanel/WaterRatioPanel";
-import TodayWaterList from "../../components/TodayWaterList/TodayWaterList";
-import MonthStatsTable from "../../components/MonthStatsTable/MonthStatsTable";
-import css from "./HomePage.module.css"
-
+// import Header from "../../components/Header/Header";
+// import DailyNorma from "../../components/DaiilyNorma/DailyNorma";
+// import WaterRatioPanel from "../../components/WaterRatioPanel/WaterRatioPanel";
+// import MonthStatsTable from "../../components/MonthStatsTable/MonthStatsTable";
 
 import css from "./HomePage.module.css";
 
 const HomePage = () => {
   return (
     <div>
-
-
-      <div className={css}>
-        <Header />
-      </div>
-      <div>
-        <DailyNorma />
-      </div>
-      <div>
-        <WaterRatioPanel />
-      </div> */}
+      <div className={css}>{/* <Header /> */}</div>
+      <div>{/* <DailyNorma /> */}</div>
+      <div>{/* <WaterRatioPanel /> */}</div>
       <div className={css.tableWrapper}>
         <TodayWaterList />
-      {/* <div>
+        {/* <div>
         <MonthStatsTable />
       </div> */}
       </div>
