62fed23a530af8f018bcfdf624b7ece5f6f6feb0
NassBay <nastiabaydina@gmail.com>
2024-08-29 12:02:42 +0300

    water/day added list with water consuming items

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import { TodayWaterList } from "../../components/TodayWaterList/TodayWaterList";
 // import Header from "../../components/Header/Header";
 // import DailyNorma from "../../components/DailyNorma/DailyNorma";
 // import WaterRatioPanel from "../../components/WaterRatioPanel/WaterRatioPanel";
 // import TodayWaterList from "../../components/TodayWaterList/TodayWaterList";
 // import MonthStatsTable from "../../components/MonthStatsTable/MonthStatsTable";
+import css from "./HomePage.module.css"
 
 // import css from "./HomePage.module.css"
 
 const HomePage = () => {
   return (
     <div>
-      <div>
+      {/* <div>
         <Header />
       </div>
       <div>
@@ -18,12 +19,12 @@ const HomePage = () => {
       </div>
       <div>
         <WaterRatioPanel />
-      </div>
-      <div>
+      </div> */}
+      <div className={css.tableWrapper}>
         <TodayWaterList />
-      </div>
-      <div>
+      {/* <div>
         <MonthStatsTable />
+      </div> */}
       </div>
     </div>
   );
