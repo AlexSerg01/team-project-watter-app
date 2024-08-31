db2b124d6faa263ea1f36a5f0b599cde5bf43458
OlenaHlu <glushenkova_es@ukr.net>
2024-08-31 17:32:54 +0300

    added tablet css for home page and fixed mobile

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -9,9 +9,10 @@ import css from "./HomePage.module.css";
 const HomePage = () => {
   return (
     <div className={css.homeMain}>
-      <div className={css.homeHeader}>{/* <Header /> */}</div>
+      <div>{/* <Header /> */}</div>
       <div className={css.homeContainer}>
         <div className={css.dailyBox}>{/* <DailyNorma /> */}</div>
+        <div className={css.homeBotle}></div>
         <div className={css.waterRatioPanel}>{/* <WaterRatioPanel /> */}</div>
         <div className={css.tableWrapper}>
           <TodayWaterList />
