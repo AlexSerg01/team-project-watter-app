9e6257cd3099d495209309e7b58bd153b389ae0d
guemplen <guemplen@gmail.com>
2024-09-01 15:10:11 +0200

    Add SettingModal component & Mobile, Tablet Styles
    Add logic validaneName to utils component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useSelector } from "react-redux";
 import {
   BrowserRouter as Router,
@@ -13,54 +12,24 @@ import HomePage from "./pages/HomePage/HomePage";
 
 // import Layout from "./components/Layout/Layout";
 // import HomePage from "./pages/HomePage/HomePage";
-// import Main from "./pages/WelcomePage/Main";
+import Main from "./pages/WelcomePage/Main";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage/ForgotPasswordPage";
 import UpdatePasswordPage from "./pages/UpdatePasswordPage/UpdatePasswordPage";
-import SettingModal from "./components/SettingModal/SettingModal";
 
 function App() {
-  const [isModalOpen, setModalOpen] = useState(false);
   const PrivateRoute = ({ children }) => {
     const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
     return isAuthenticated ? children : <Navigate to="/signin" />;
   };
 
-  const handleSave = (data) => {
-    console.log("Saving user data...", data);
-    setModalOpen(false); // Закрытие модалки после успешного сохранения
-  };
-
   return (
     <Layout>
       <Router>
         <Routes>
-          {/* <Route path="/welcome" element={} /> */}
+          <Route path="/welcome" element={<Main />} />
           <Route path="/signup" element={<SignupPage />} />
-          <Route
-            path="/signin"
-            element={
-              <>
-                <SigninPage />
-                {/* Кнопка для открытия модального окна */}
-                <button onClick={() => setModalOpen(true)}>
-                  Open Settings
-                </button>
-                {/* Модальное окно */}
-                <SettingModal
-                  isOpen={isModalOpen}
-                  onClose={() => setModalOpen(false)}
-                  userData={{
-                    photo: "",
-                    gender: "male",
-                    name: "David",
-                    email: "david401@gmail.com",
-                  }}
-                  onSave={handleSave}
-                />
-              </>
-            }
-          />
+          <Route path="/signin" element={<SigninPage />} />
           <Route
             path="/home"
             element={<PrivateRoute>{<HomePage />}</PrivateRoute>}
@@ -69,10 +38,7 @@ function App() {
           <Route path="/" element={<Navigate to="/home" />} />
           {/* Add more routes as necessary */}
           <Route path="/reset-password" element={<ForgotPasswordPage />} />
-          <Route
-            path="/reset-password/:verificationToken"
-            element={<UpdatePasswordPage />}
-          />
+          <Route path="/reset-password1" element={<UpdatePasswordPage />} />
         </Routes>
       </Router>
     </Layout>
