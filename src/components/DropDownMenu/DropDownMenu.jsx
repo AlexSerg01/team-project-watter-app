3e92f3ee8c58243f3f9e3d2e1f0730ff6ad3fd42
guemplen <guemplen@gmail.com>
2024-09-03 01:20:13 +0200

    Update component SettingModal
    Add styles for Desktop.
    Add reading and writing to State.
    Add a modal call from DropDownMenu.
    Add functions for integrating modals with the API in Fetch.
    Moved validation files to the SettingModal component folder

--- src/components/DropDownMenu/DropDownMenu.jsx
+++ src/components/DropDownMenu/DropDownMenu.jsx
@@ -1,19 +1,28 @@
 import { useState, useEffect, useRef } from "react";
 import avatar from "../../assets/images/avatar.png";
-import { DropDownContainer, DropDownBtn, UserName, UserAvatar, ChevronIcon, Menu, MenuItem} from "./DropDownMenu.styled";
+import {
+  DropDownContainer,
+  DropDownBtn,
+  UserName,
+  UserAvatar,
+  ChevronIcon,
+  Menu,
+  MenuItem,
+} from "./DropDownMenu.styled";
 import icons from "../../assets/icons.svg";
+import SettingModal from "../SettingModal/SettingModal";
 
 export const DropDownMenu = () => {
   //все пов'язане з юзером замінити на данні з БД та видалити картинку для аватар
-  const userName = 'David';
-  const userAvatar = avatar; 
+  const userName = "David";
+  const userAvatar = avatar;
 
   const [isOpen, setIsOpen] = useState(false);
+  const [isSettingModalOpen, setIsSettingModalOpen] = useState(false);
   const menuRef = useRef(null);
 
-
   const toggleMenu = () => {
-    setIsOpen(prevState => !prevState);
+    setIsOpen((prevState) => !prevState);
   };
 
   const handleClickOutside = (event) => {
@@ -24,36 +33,46 @@ export const DropDownMenu = () => {
 
   useEffect(() => {
     if (isOpen) {
-      document.addEventListener('mousedown', handleClickOutside);
+      document.addEventListener("mousedown", handleClickOutside);
     }
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [isOpen]);
 
   const handleMenuItemClick = (action) => {
+    if (action === "setting") {
+      setIsSettingModalOpen(true);
+    } else if (action === "logout") {
+      // Додати логіку для вихіда из аккаунта
+    }
     console.log(action);
     setIsOpen(false);
-  }
+  };
 
   return (
     <DropDownContainer ref={menuRef}>
       <DropDownBtn onClick={toggleMenu}>
         <UserName>{userName}</UserName>
-        <UserAvatar src={userAvatar} alt="User`s Avatar" width="28" height="28" ></UserAvatar>
+        <UserAvatar
+          src={userAvatar}
+          alt="User`s Avatar"
+          width="28"
+          height="28"
+        ></UserAvatar>
         <ChevronIcon>
-                <use href={`${icons}#icon-chevron-down`} /> 
+          <use href={`${icons}#icon-chevron-down`} />
         </ChevronIcon>
       </DropDownBtn>
       {isOpen && (
         <Menu>
-          <MenuItem onClick={() => handleMenuItemClick('setting')}>
+          <MenuItem onClick={() => handleMenuItemClick("setting")}>
             <svg width="16" height="16">
               <use href={`${icons}#icon-setting`} />
             </svg>
             Setting
           </MenuItem>
-          <MenuItem onClick={() => handleMenuItemClick('logout')}>
+          <MenuItem onClick={() => handleMenuItemClick("logout")}>
             <svg width="16" height="16">
               <use href={`${icons}#icon-logout`} />
             </svg>
@@ -61,6 +80,17 @@ export const DropDownMenu = () => {
           </MenuItem>
         </Menu>
       )}
+      <SettingModal
+        isOpen={isSettingModalOpen}
+        onClose={() => setIsSettingModalOpen(false)}
+        userData={{
+          photo: userAvatar,
+          name: userName,
+          email: "david401@gmail.com",
+          gender: "male",
+        }}
+        onSave={(data) => console.log("Saved data:", data)}
+      />
     </DropDownContainer>
-  )
+  );
 };
--- src/fetch/fetch.js
+++ src/fetch/fetch.js
@@ -1,27 +1,54 @@
-import axios from 'axios'
+import axios from "axios";
+
+const API_URL = "https://team-project-b-watter-app.onrender.com";
 
 export function setAuthHeader(token) {
-  axios.defaults.headers.common.Authorization = token
+  axios.defaults.headers.common.Authorization = token;
 }
 
 export async function registerFetch(data) {
-  axios.defaults.baseURL = '/'
-  const response = await axios.post('users/signup', data)
-  return response
+  axios.defaults.baseURL = "/";
+  const response = await axios.post("users/signup", data);
+  return response;
 }
 export const sendForgotPasswordEmail = async (email) => {
   try {
-    const response = await axios.post('users/forgot-password', { email })
-    return response.data
+    const response = await axios.post("users/forgot-password", { email });
+    return response.data;
   } catch (error) {
-    throw new Error(error.response?.data?.message || 'Failed to send email')
+    throw new Error(error.response?.data?.message || "Failed to send email");
   }
-}
+};
 export const UpdatePassword = async (token, newPassword) => {
-  const response = await axios.post('users/update-password', {
+  const response = await axios.post("users/update-password", {
     token,
     newPassword,
-  })
+  });
 
-  return response
+  return response;
+};
+
+// Функция для получения данных пользователя
+export async function getUserInfo(token) {
+  setAuthHeader(token);
+  const response = await axios.get(`${API_URL}/user/info`);
+  return response;
+}
+
+// Функция для обновления данных пользователя
+export async function updateUserInfo(token, data) {
+  setAuthHeader(token);
+  const response = await axios.patch(`${API_URL}/user/updateInfo`, data);
+  return response;
+}
+
+// Функция для обновления аватара пользователя
+export async function updateUserPhoto(token, formData) {
+  setAuthHeader(token);
+  const response = await axios.patch(`${API_URL}/user/userPhoto`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+  return response;
 }
