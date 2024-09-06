de150df3bcabdd79e2d58e525f5ded6fb02273ef
OlenaHlu <glushenkova_es@ukr.net>
2024-09-07 00:23:55 +0300

    add user

---
+++ src/redux/user/selectors.js
@@ -0,0 +1,3 @@
+export const selectUserGender = (state) => state.user.gender;
+export const selectWaterRateUpdate = (state) => state.user.waterRate;
+export const selectLoadingUserData = (state) => state.user.loading;
