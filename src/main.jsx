ec48d20be2b93882d98fa28a2b44d7e182524e48
AlexSerg01 <alex.box.work@icloud.com>
2024-09-06 09:51:25 +0200

    add persist

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import store from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,34 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // localStorage буде використовуватись за замовчуванням
 import authReducer from "./auth/slice";
 import waterInfoReducer from "./waterInfo/waterSlice";
 import { dailyNormaReducer } from "./dailyNorma/slice";
 import { waterReducer } from "./water/waterSlice";
 
+// Конфігурація для persist
+const persistConfig = {
+  key: "root", // ключ для збереження стану
+  storage, // сховище, яке буде використовуватись
+  whitelist: ["auth"], // вкажіть ред'юсери, які ви хочете зберігати
+};
 
-const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    waterInfo: waterInfoReducer,
-    norma: dailyNormaReducer,
-    water: waterReducer
-  },
+// Об'єднання ред'юсерів
+const rootReducer = combineReducers({
+  auth: authReducer,
+  waterInfo: waterInfoReducer,
+  norma: dailyNormaReducer,
+  water: waterReducer,
 });
 
+// persistReducer для збереження стану
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Налаштування store з persistedReducer
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+// Експортуємо persistor
+export const persistor = persistStore(store);
 export default store;
