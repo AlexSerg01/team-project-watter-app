41313658c597f6fb1f29ec2cd3ce5aa23f492ecc
AlexSerg01 <alex.box.work@icloud.com>
2024-09-07 18:11:15 +0200

    user

--- src/redux/user/operations.js
+++ src/redux/user/operations.js
@@ -1,13 +1,12 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-// Асинхронні Thunk-и
 export const getUserData = createAsyncThunk(
   "user/getUserData",
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get("/users/info");
-      return response.data;
+      const response = await axios.get("/user/info");
+      return response.data.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
@@ -18,7 +17,7 @@ export const patchUserPhoto = createAsyncThunk(
   "user/patchUserPhoto",
   async (data, thunkAPI) => {
     try {
-      const response = await axios.patch("/user/userPhoto", data);
+      const response = await axios.patch("user/userPhoto", data);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -30,7 +29,7 @@ export const patchUserInfo = createAsyncThunk(
   "user/patchUserInfo",
   async (data, thunkAPI) => {
     try {
-      const response = await axios.patch("/user/updateInfo", data);
+      const response = await axios.patch("user/updateInfo", data);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -42,7 +41,7 @@ export const patchDailyWater = createAsyncThunk(
   "user/patchDailyWater",
   async (data, thunkAPI) => {
     try {
-      const response = await axios.patch("/user/newDailyWaterIntake", data);
+      const response = await axios.patch("user/newDailyWaterIntake", data);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
